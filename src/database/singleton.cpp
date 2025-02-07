5770e6e25d35f1f32f0b205184a55990fefbbf39
kritomas <Unknown>
2025-02-07 08:26:33 +0100

    Fixed errors on windows.

--- src/database/singleton.cpp
+++ src/database/singleton.cpp
@@ -4,8 +4,6 @@
 
 const char* DB_PATH = "interbanqa.db";
 
-const std::string SCHEMA = kritase64::decodeToString("YmVnaW4gdHJhbnNhY3Rpb247CgpjcmVhdGUgdGFibGUgQWNjb3VudAooCglpZCBpbnRlZ2VyIHByaW1hcnkga2V5IGNoZWNrKGlkIGJldHdlZW4gOTk5OSBhbmQgOTk5OTkpLAoJYmFsYW5jZSBiaWdpbnQgbm90IG51bGwgZGVmYXVsdCAwIGNoZWNrKGJhbGFuY2UgPj0gMCksCgoJaXNfZHVtbXkgYml0IG5vdCBudWxsIGRlZmF1bHQgMAopOwoKaW5zZXJ0IGludG8gQWNjb3VudCAoaWQsIGlzX2R1bW15KSB2YWx1ZXMgKDk5OTksIDEpOyAtLSBEdW1teSBpbnNlcnQKCmNyZWF0ZSB2aWV3IEJhbGFuY2VfVG90YWwgYXMKCXNlbGVjdCBpZm51bGwoc3VtKGJhbGFuY2UpLCAwKSBmcm9tIEFjY291bnQgd2hlcmUgaXNfZHVtbXkgPSAwOwoKY3JlYXRlIHZpZXcgQWNjb3VudF9Ub3RhbCBhcwoJc2VsZWN0IGNvdW50KCopIGZyb20gQWNjb3VudCB3aGVyZSBpc19kdW1teSA9IDA7Cgpjb21taXQ7");
-
 DBSingleton::DBSingleton()
 {
 	std::ifstream file(DB_PATH);
@@ -24,6 +22,7 @@ void DBSingleton::reset()
 	std::fstream file(DB_PATH, std::ios::out | std::ios::trunc | std::ios::binary);
 	file.close();
 
+	const std::string SCHEMA = kritase64::decodeToString("YmVnaW4gdHJhbnNhY3Rpb247CgpjcmVhdGUgdGFibGUgQWNjb3VudAooCglpZCBpbnRlZ2VyIHByaW1hcnkga2V5IGNoZWNrKGlkIGJldHdlZW4gOTk5OSBhbmQgOTk5OTkpLAoJYmFsYW5jZSBiaWdpbnQgbm90IG51bGwgZGVmYXVsdCAwIGNoZWNrKGJhbGFuY2UgPj0gMCksCgoJaXNfZHVtbXkgYml0IG5vdCBudWxsIGRlZmF1bHQgMAopOwoKaW5zZXJ0IGludG8gQWNjb3VudCAoaWQsIGlzX2R1bW15KSB2YWx1ZXMgKDk5OTksIDEpOyAtLSBEdW1teSBpbnNlcnQKCmNyZWF0ZSB2aWV3IEJhbGFuY2VfVG90YWwgYXMKCXNlbGVjdCBpZm51bGwoc3VtKGJhbGFuY2UpLCAwKSBmcm9tIEFjY291bnQgd2hlcmUgaXNfZHVtbXkgPSAwOwoKY3JlYXRlIHZpZXcgQWNjb3VudF9Ub3RhbCBhcwoJc2VsZWN0IGNvdW50KCopIGZyb20gQWNjb3VudCB3aGVyZSBpc19kdW1teSA9IDA7Cgpjb21taXQ7");
 	sqlite::database db(DB_PATH);
 	sqlite3_exec(db.connection().get(), SCHEMA.c_str(), nullptr, nullptr, nullptr);
 }
